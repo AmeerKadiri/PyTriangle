f12a7554095f4d1132717af716c0cb8d6f63ab24
AmeerKadiri <134822905+AmeerKadiri@users.noreply.github.com>
2023-06-12 22:37:45 -0600

    Add files via upload

---
+++ temp.py
@@ -0,0 +1,8 @@
+number = int(input("Enter a number: "))
+if number > 0:
+    print("Number is above 0. Terminating the code.")
+    exit()
+else:
+    # Continue with the rest of your code here
+    print("Number is not above 0. Proceeding with the code.")
+    # Additional code logic goes here
